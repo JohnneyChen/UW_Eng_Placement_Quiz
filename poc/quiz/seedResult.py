eca89d351db23d25b62f813a4b1659f99af6ed3f
Johnney Chen <mzchenca@gmail.com>
2021-03-04 09:14:09 -0700

    fixed seedResult.py

--- poc/quiz/seedResult.py
+++ poc/quiz/seedResult.py
@@ -1,34 +1,36 @@
 from .models import *
 from random import randint, shuffle
-from datetime import datetime
+import datetime
 
-d1 = datetime.strptime('1/1/2015 1:30 PM', '%m/%d/%Y %I:%M %p')
-d2 = datetime.strptime('1/1/2021 4:50 AM', '%m/%d/%Y %I:%M %p')
+d1 = datetime.datetime.strptime('1/1/2015 1:30 PM', '%m/%d/%Y %I:%M %p')
+d2 = datetime.datetime.strptime('1/1/2021 4:50 AM', '%m/%d/%Y %I:%M %p')
 
 def random_date(start, end):
-    return start + datetime.timedelta( seconds = randint(0,int((end-start).total_seconds()))
+    return start + datetime.timedelta( seconds=randint(0, int((end - start).total_seconds())),
+    )
 
-programList = list()
+programs = list()
+
 for program in Program.objects.all():
-    programList.append(program)
+    programs.append(program)
 
 for i in range(5000):
     shuffle(programList)
     res = Result()
-    res.one = programList[0]
-    res.two = programList[1]
-    res.three = programList[2]
-    res.four = programList[3]
-    res.five = programList[4]
-    res.six = programList[5]
-    res.seven = programList[6]
-    res.eight = programList[7]
-    res.nine = programList[8]
-    res.ten = programList[9]
-    res.eleven = programList[10]
-    res.twelve = programList[11]
-    res.thirteen = programList[12]
-    res.fourteen = programList[13]
-    res.fifteen = programList[14]
+    res.one = programs[0]
+    res.two = programs[1]
+    res.three = programs[2]
+    res.four = programs[3]
+    res.five = programs[4]
+    res.six = programs[5]
+    res.seven = programs[6]
+    res.eight = programs[7]
+    res.nine = programs[8]
+    res.ten = programs[9]
+    res.eleven = programs[10]
+    res.twelve = programs[11]
+    res.thirteen = programs[12]
+    res.fourteen = programs[13]
+    res.fifteen = programs[14]
     res.time = random_date(d1,d2)
     res.save()
