7b2fef61bd696b2e5b8d4fbe303a5279201829bb
johnneychen <mzchenca@gmail.com>
2021-04-21 07:48:00 -0600

    changed result export to streaming for export of larger files

--- poc/quiz/views.py
+++ poc/quiz/views.py
@@ -1,6 +1,6 @@
 import csv
 from datetime import datetime
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, StreamingHttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
@@ -484,10 +484,12 @@ def download_results(request):
     if request.method == 'GET':
         before = request.GET.get('before')
         after = request.GET.get('after')
-        response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename="EngineeringQuizResults.csv"'
+        # response = HttpResponse(content_type='text/csv')
+        # response['Content-Disposition'] = 'attachment; filename="EngineeringQuizResults.csv"'
 
-        writer = csv.writer(response)
+        # writer = csv.writer(response)
+
+
         
         writer.writerow(['Result id', 'Quiz taken date', 'First recommendation', 'Second recommendation', 'Third recommendation','Fourth recommendation','Fifth recommendation','Sixth recommendation','Seventh recommendation','Eighth recommendation','Ninth recommendation','Tenth recommendation','Eleventh recommendation','Twelfth recommendation','Thirteenth recommendation','Fourteenth recommendation', 'Fifteenth recommendation'])
         
@@ -498,7 +500,14 @@ def download_results(request):
         if after != '':
             results = results.filter(time__gte=after)
 
-        for result in results:
-            writer.writerow([result.id, result.time, result.one.program_name, result.two.program_name, result.three.program_name, result.four.program_name, result.five.program_name, result.six.program_name, result.seven.program_name, result.eight.program_name, result.nine.program_name, result.ten.program_name, result.eleven.program_name, result.twelve.program_name, result.thirteen.program_name, result.fourteen.program_name, result.fifteen.program_name])
+        # for result in results:
+        #     writer.writerow([result.id, result.time, result.one.program_name, result.two.program_name, result.three.program_name, result.four.program_name, result.five.program_name, result.six.program_name, result.seven.program_name, result.eight.program_name, result.nine.program_name, result.ten.program_name, result.eleven.program_name, result.twelve.program_name, result.thirteen.program_name, result.fourteen.program_name, result.fifteen.program_name])
 
-        return response
+        pseudo_buffer = Echo()
+        writer = csv.writer(pseudo_buffer)
+
+        return StreamingHttpResponse((writer.writerow([result.id, result.time, result.one.program_name, result.two.program_name, result.three.program_name, result.four.program_name, result.five.program_name, result.six.program_name, result.seven.program_name, result.eight.program_name, result.nine.program_name, result.ten.program_name, result.eleven.program_name, result.twelve.program_name, result.thirteen.program_name, result.fourteen.program_name, result.fifteen.program_name]) for result in results), content_type='text/csv', headers={'Content-Disposition':"attachment;filename='results.csv'})
+
+class Echo:
+    def write(self, value):
+        return value
