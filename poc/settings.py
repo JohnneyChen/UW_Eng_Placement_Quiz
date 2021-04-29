7a81567c6be3ea46111d3c43427e81f942440994
johnneychen <mzchenca@gmail.com>
2021-04-29 09:37:43 -0600

    b

--- poc/settings.py
+++ poc/settings.py
@@ -138,10 +138,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'poc/quiz/static'),
-   ]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
